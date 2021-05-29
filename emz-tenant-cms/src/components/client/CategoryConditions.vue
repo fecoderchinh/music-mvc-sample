a12def4fb47810c4f70594b5d81c8ce84c12a2ce
Chinh Trung <fecoder.chinh@gmail.com>
2021-05-29 21:27:23 +0700

    them su kien click cho radio screen danh muc dieu kien

--- emz-tenant-cms/src/components/client/CategoryConditions.vue
+++ emz-tenant-cms/src/components/client/CategoryConditions.vue
@@ -12,50 +12,64 @@
       <div class="w-full mb-5">
         <div class="flex flex-wrap">
 
-          <Radio id="radio-category-condition-1" name="radio-category-condition" label-class="text-labelAndTitle mr-3 md:mr-8 sm:mb-3">
+          <Radio
+              id="radio-category-condition-1"
+              name="radio-category-condition"
+              label-class="text-labelAndTitle mr-3 md:mr-8 sm:mb-3"
+              @change="auto = true" :checked="auto">
             <template slot="radio-text">Tự động thêm sản phẩm</template>
           </Radio>
 
-          <Radio id="radio-category-condition-2" name="radio-category-condition" label-class="text-labelAndTitle" checked>
+          <Radio
+              id="radio-category-condition-2"
+              name="radio-category-condition"
+              label-class="text-labelAndTitle mr-3 md:mr-8 sm:mb-3"
+              @change="auto = false" :checked="!auto">
             <template slot="radio-text">Thêm sản phẩm thủ công</template>
           </Radio>
 
         </div>
       </div>
 
-      <div class="table w-full">
-        <div class="table-row w-full">
-          <div class="table-cell">
-            <div class="grid grid-cols-4 sm:grid-cols-1 gap-5">
-              <div class="col-span-1">
-                <SelectOption :option-data="this.optionDataSelect1"/>
-              </div>
-              <div class="col-span-1">
-                <SelectOption :option-data="this.optionDataSelect2"/>
-              </div>
-              <div class="col-span-2 bg-gray-300">
-                <InputType
-                    id="add-product-name"
-                    name="add-product-name"
-                    button="reset"
-                    input-class="cms-input__default">
-                </InputType>
+      <div class="w-full" v-if="auto">
+        <div class="table w-full">
+          <div class="table-row w-full">
+            <div class="table-cell">
+              <div class="grid grid-cols-4 sm:grid-cols-1 gap-5">
+                <div class="col-span-1">
+                  <SelectOption :option-data="this.optionDataSelect1"/>
+                </div>
+                <div class="col-span-1">
+                  <SelectOption :option-data="this.optionDataSelect2"/>
+                </div>
+                <div class="col-span-2 bg-gray-300">
+                  <InputType
+                      id="add-product-name"
+                      name="add-product-name"
+                      button="reset"
+                      input-class="cms-input__default">
+                  </InputType>
+                </div>
               </div>
             </div>
+            <div class="w-auto text-center text-placeholderStyle ml-3">
+              <DeleteSVG class="w-4 inline -mt-1 fill-current"/>
+            </div>
           </div>
-          <div class="w-auto text-center text-placeholderStyle ml-3">
-            <DeleteSVG class="w-4 inline -mt-1 fill-current"/>
+        </div>
+
+        <div class="grid grid-cols-6 gap-5 mt-5">
+          <div class="col-span-6 md:col-span-2">
+            <DropdownCondition/>
+          </div>
+          <div class="col-span-6 md:col-span-2 flex items-center">
+            <ModalDsThoaMan/>
           </div>
         </div>
       </div>
 
-      <div class="grid grid-cols-6 gap-5 mt-5">
-        <div class="col-span-6 md:col-span-2">
-          <DropdownCondition/>
-        </div>
-        <div class="col-span-6 md:col-span-2 flex items-center">
-          <ModalDsThoaMan/>
-        </div>
+      <div class="w-full" v-else>
+        <TableConditionAuto/>
       </div>
 
     </template>
@@ -69,6 +83,7 @@ import SelectOption from '@/components/client/SelectOption.vue';
 import InputType from '@/components/client/InputType.vue';
 import DropdownCondition from '@/components/client/DropdownCondition.vue';
 import ModalDsThoaMan from "@/components/client/ModalDsThoaMan";
+import TableConditionAuto from "@/components/client/TableConditionAuto";
 
 import {
   DeleteSVG,
@@ -82,10 +97,12 @@ export default {
     InputType,
     DeleteSVG,
     DropdownCondition,
-    ModalDsThoaMan
+    ModalDsThoaMan,
+    TableConditionAuto
   },
   data() {
     return {
+      auto: false,
       optionDataSelect1: [
         { name: 'Tên sản phẩm' },
         { name: 'aaa' },
