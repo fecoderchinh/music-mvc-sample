2efb5f4f24901402aea4927ed6c31d524d88c752
Quyen Tran <quyenduy@icsc.vn>
2021-06-08 22:56:24 +0700

    initialize swagger for tenant-api

--- emz-backend/apps/tenant-api/src/main.ts
+++ emz-backend/apps/tenant-api/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from "@nestjs/config";
 import { TenantApiModule } from './tenant-api.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as mongoose from "mongoose";
 
 async function bootstrap() {
   const app = await NestFactory.create(TenantApiModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Emz Tenant API')
+    .setVersion('v1')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('emz')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   await app.listen(port, () => {
