4b9b83b462c0a52da2b969a135d031f148b0fd74
Chinh Trung <fecoder.chinh@gmail.com>
2021-06-26 21:30:51 +0700

    update settings icons

--- emz-tenant-cms/src/components/client/Settings.vue
+++ emz-tenant-cms/src/components/client/Settings.vue
@@ -5,8 +5,8 @@
       <div class="col-span-2 sm:col-span-6" v-for="(data, index) in optionData" :key="index">
         <div class="grid grid-cols-4 gap-5 p-10px group hover:bg-sidebarHover">
           <div class="col-span-1">
-            <div class="w-50px h-50px flex items-center bg-gray-400 justify-center" v-if="data.component">
-              <component v-bind:is="data.component" class="w-8 h-8 fill-menuIcon group-hover:fill-buttonAndURL hover:filter-none" />
+            <div class="w-50px h-50px flex items-center bg-gray-400 justify-center text-menuIcon" v-if="data.component">
+              <component v-bind:is="data.component" class="w-8 h-8" />
             </div>
           </div>
           <div class="col-span-3">
@@ -27,73 +27,67 @@
 import Box from '@/components/client/Box.vue';
 
 import {
+  TruckFillSVG,
   TruckSVG,
-  TruckCashSVG,
   PaymentSVG,
   GlobeWWWSVG,
   LocationSVG,
   AccountCircleSVG,
   SettingSVG,
+  CheckedFileSVG
 } from '@/components/SVGs.vue';
 
 export default {
   components: {
-    Box,
-    TruckSVG,
-    TruckCashSVG,
-    PaymentSVG,
-    GlobeWWWSVG,
-    LocationSVG,
-    AccountCircleSVG,
-    SettingSVG,
+    Box
   },
   data() {
     return {
       optionData: [
         {
-          component: 'TruckSVG',
+          component: TruckFillSVG,
           title: 'Kết nối vận chuyển',
           content: 'Kết nối và quản lý các phương thức vận chuyển đơn hàng',
           url: '/dashboard/shipping',
         },
         {
-          component: 'TruckCashSVG',
+          component: TruckSVG,
           title: 'Phí vận chuyển trên website',
           content: 'Quản lý các mức phí vận chuyển trên website',
           url: '/dashboard/shipping-fee-settings',
         },
         {
-          component: 'PaymentSVG',
+          component: PaymentSVG,
           title: 'Kết nối thanh toán',
           content: 'Kết nối và quản lý các phương thức thanh toán trên website',
           url: '/dashboard/connect-payment',
         },
         {
-          component: 'GlobeWWWSVG',
+          component: GlobeWWWSVG,
           title: 'Tên miền riêng',
           content: 'Kết nối tên miền riêng cho website',
           url: '/dashboard/domain',
         },
         {
-          component: 'LocationSVG',
+          component: LocationSVG,
           title: 'Chi nhánh & Kho hàng',
           content: 'Quản lý địa chỉ cửa hàng, chi nhánh, kho hàng',
           url: '/dashboard/warehouse',
         },
         {
-          component: 'AccountCircleSVG',
+          component: AccountCircleSVG,
           title: 'Tài khoản & Phân quyền',
           content: 'Quản lý các tài khoản nhân viên và phân quyền',
           url: '/dashboard/user-roles',
         },
         {
-          component: 'SettingSVG',
+          component: SettingSVG,
           title: 'Cài đặt chung',
           content: 'Thông tin cửa hàng, cài đặt quảng cáo, tùy chọn…',
           url: '/dashboard/settings-general',
         },
         {
-          component: 'SettingSVG',
+          component: CheckedFileSVG,
           title: 'Gói dịch vụ',
           content: 'Các gói đang sử dụng, cung cấp các tuỳ chọn nâng cấp…',
           url: '/dashboard/package',
