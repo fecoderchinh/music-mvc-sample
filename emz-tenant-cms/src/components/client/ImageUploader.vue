fe12c6e72f97580f1166bf3574042ed6285429e4
Chinh Trung <fecoder.chinh@gmail.com>
2021-06-30 08:35:00 +0700

    update ImageUploader

--- emz-tenant-cms/src/components/client/ImageUploader.vue
+++ emz-tenant-cms/src/components/client/ImageUploader.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- eslint-disable max-len -->
   <div class="flex items-center justify-center bg-white">
-    <template v-if="image">
-      <img :src="image" alt="" class="w-full object-contain"/>
-    </template>
-    <div class="px-5" v-else>
-      <SquareSVG class="w-20 h-20 mb-5 mx-auto fill-placeholderStyle hover:fill-placeholderStyle"/>
+    <div class="text-menuIcon">
+      <template v-if="image">
+        <img :src="image" alt="" class="w-full object-contain mb-3"/>
+      </template>
+      <NoImgSVG v-else class="w-20 h-20 mb-5 mx-auto"/>
       <Button button-class="cms-button cms-button-blue" @click="$emit('click')">
         <template slot="name">
           Upload ảnh
@@ -19,13 +19,13 @@
 import Button from '@/components/client/Button.vue';
 
 import {
-  SquareSVG,
+  NoImgSVG,
 } from '../SVGs.vue';
 
 export default {
   components: {
     Button,
-    SquareSVG,
+    NoImgSVG,
   },
   props: ['image']
 };
